2de9430b1871fe1e5ef10f97ca4924a3e2e011b3
nafees-lang <nafees@milquesingapore.com>
2025-09-02 16:50:36 +0530

    Update AdminPanel.tsx

--- components/AdminPanel.tsx
+++ components/AdminPanel.tsx
@@ -1,7 +1,7 @@
 
 'use client'
 import { useState } from 'react'
-import { db } from '@/lib/firebase'
+import { db } from './lib/firebase'
 import { doc, getDoc, updateDoc, arrayUnion, Timestamp } from 'firebase/firestore'
 
 export default function AdminPanel() {
